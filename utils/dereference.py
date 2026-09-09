1228b6506362c9652b34dc8ea794e709402178c3
Brendan Reardon <9423424+brendanreardon@users.noreply.github.com>
2026-09-09 13:27:51 -0400

    Updated therapy ids

--- utils/dereference.py
+++ utils/dereference.py
@@ -531,7 +531,7 @@ class Propositions(BaseTable):
             KeyError: If neither referenced_key values, `therapy_id` or `therapy_group_id, are not found in a record.
         """
         for record in self.records:
-            if isinstance(record["therapy_id"], int):
+            if isinstance(record["therapy_id"], str):
                 self.dereference_single(
                     record=record,
                     referenced_key="therapy_id",
